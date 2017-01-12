bfe5bad0a6ed479cbd2c5c008da91f19d7224fc6
davidmueller13 <david.mueller1013@gmail.com>
2017-01-12 17:34:23 +0100

    Revert "Nougat MR1: Advanced restart menu"
    This reverts commit d3ca1218cb5b2bf54ff5ad2297859cba41e01131.
    
    # Conflicts:
    #	core/res/res/values/symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6264,12 +6264,6 @@ public final class Settings {
         public static final String CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED =
                 "camera_double_tap_power_gesture_disabled";
 
-	/**
-         * Whether to include options in power menu for rebooting into recovery or bootloader
-         * @hide
-         */
-        public static final String ADVANCED_REBOOT = "advanced_reboot";
-
         /**
          * Whether the camera double twist gesture to flip between front and back mode should be
          * enabled.
@@ -6519,8 +6513,7 @@ public final class Settings {
             QS_TILES,
             DOZE_ENABLED,
             DOZE_PULSE_ON_PICK_UP,
-            DOZE_PULSE_ON_DOUBLE_TAP,
-            ADVANCED_REBOOT
+            DOZE_PULSE_ON_DOUBLE_TAP
         };
 
         /**
--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -354,7 +354,11 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         @Override
         public boolean onLongPress() {
-            // noop
+            UserManager um = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
+            if (!um.hasUserRestriction(UserManager.DISALLOW_SAFE_BOOT)) {
+                mWindowManagerFuncs.rebootSafeMode(true);
+                return true;
+            }
             return false;
         }
 
@@ -402,7 +406,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         @Override
         public void onPress() {
-            mWindowManagerFuncs.reboot(true /* confirm */);
+            mWindowManagerFuncs.reboot(false /* confirm */);
         }
     }
 
